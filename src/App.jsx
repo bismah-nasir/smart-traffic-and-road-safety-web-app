916dd2b9368511caf76a4c227380ac1d4e2f025b
Bismah <bismahnasir000@gmail.com>
2026-07-16 16:27:14 +0500

    feat: implement LoadingScreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useState, useEffect } from "react";
+
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-function App() {
+import LoadingScreen from "@/components/LoadingScreen";
+
+export default function App() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setIsLoading(false), 2800);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <>
-            <Navbar />
-        
-            <Footer />
+            <LoadingScreen isLoading={isLoading} />
+
+            {!isLoading && (
+                <>
+                    <Navbar />
+
+                    <section className="relative min-h-screen flex flex-col items-center justify-center overflow-hidden">
+                        <h1>Smart Traffic and Road Safety Web App</h1>
+                    </section>
+
+                    <Footer />
+                </>
+            )}
         </>
     );
 }
-
-export default App;
